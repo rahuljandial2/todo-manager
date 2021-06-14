9a06b8a514b2f6471dd75c7cd423422d77074a7e
rahuljandial2 <61629739+rahuljandial2@users.noreply.github.com>
2021-06-14 18:20:15 +0530

    Added function to app.js
    Converted the App component to a react class component.
    Added onChange event listener on the input field.
    added addTask event listner on the button(It doesn't do anything yet).

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { Component } from 'react';
 import './App.css';
-import { useState } from 'react';
 
-function App() {
-  const [task, setTask] = useState();
-  return (
-    <div className="App">
-      <section>
-        <input type="text" placeholder="Todo title" onChange={task => setTask(task)} />
-      </section>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: '' };
+    this.addTask = this.addTask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  addTask() {
+    console.log(this.state.task);
+    this.setState({ task: "" })
+  }
+  handleChange(event) {
+    this.setState({ task: event.target.value });
+  }
+  render() {
+    return (
+      <div className="App">
+        <input className="App-task-input" type="text" placeholder="Title"
+          value={this.state.task} onChange={this.handleChange} />
+        <button className="App-add-btn" onClick={this.addTask}>Add Task</button>
+      </div>
+    );
+  }
 }
-
 export default App;
